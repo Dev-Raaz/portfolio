5d2a904902ebcd4f2c08d23cdfab32bbc6eaff88
Dev-Raaz <singhdebraj51@gmail.com>
2022-09-20 08:15:10 +0530

    Trying something

--- src/components/pages/home/LetsConnect.js
+++ src/components/pages/home/LetsConnect.js
@@ -13,25 +13,29 @@ const LetsConnect = () => {
             <a href='https://www.instagram.com/dev.webdev'
             target='_blank'
             rel='noreferrer' 
-            className='social' data-aos='fade-up' data-aos-delay='400'>
+            className='social' data-aos='fade-up' 
+            data-aos-delay='400' data-aos-offset='-50'>
               <img src='/res/socials/instagram.svg' alt='Instagram Logo'/>
             </a>
             <a href='https://www.linkedin.com/in/debrajsingh'
             target='_blank'
             rel='noreferrer' 
-            className='social' data-aos='fade-up' data-aos-delay='500'>
+            className='social' data-aos='fade-up' 
+            data-aos-delay='500' data-aos-offset='-50'>
               <img src='/res/socials/linkedin.svg' alt='LinkedIn Logo'/>
             </a>
             <a href='https://twitter.com/tweetingwebdev'
             target='_blank'
             rel='noreferrer' 
-            className='social' data-aos='fade-up' data-aos-delay='600'>
+            className='social' data-aos='fade-up' 
+            data-aos-delay='600' data-aos-offset='-50'>
               <img src='/res/socials/twitter.svg' alt='Twitter Logo'/>
             </a>
             <a href='https://devwebdev.hashnode.dev'
             target='_blank'
             rel='noreferrer' 
-            className='social' data-aos='fade-up' data-aos-delay='700'>
+            className='social' data-aos='fade-up' 
+            data-aos-delay='700' data-aos-offset='-50'>
               <img src='/res/socials/hashnode.svg' alt='Hashnode Logo'/>
             </a>
           </div>
